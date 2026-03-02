b7d3d1a64723cb15c83e436d3da1991f5b0daac9
CONORSHAUGHNESSY-STU <g00432599@atu.ie>
2026-03-02 12:14:33 +0000

    Booking Controller - Start Made With RestController & Get Mapping

--- src/main/java/ie/atu/exam_summer/oop_exam_summer_50/BookingController/BookingController.java
+++ src/main/java/ie/atu/exam_summer/oop_exam_summer_50/BookingController/BookingController.java
@@ -1,4 +1,20 @@
 package ie.atu.exam_summer.oop_exam_summer_50.BookingController;
 
+import ie.atu.exam_summer.oop_exam_summer_50.*;
+import ie.atu.exam_summer.oop_exam_summer_50.BookingModel.BookingModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class BookingController {
+
+    @GetMapping("/bookingID")
+    public String getBookingID(@PathVariable Long bookingID, String roomNumber, String studentEmail, double bookingDate, double startHour, double durationHour) {
+        return "Booking ID: " + bookingID + " Room Number: " + roomNumber + " Student Email: " + studentEmail + " Booking Date: " + bookingDate + " Start Hour: " + startHour + " Duration Hours: " + durationHour;
+    }
 }
+
--- src/main/java/ie/atu/exam_summer/oop_exam_summer_50/BookingService/BookingService.java
+++ src/main/java/ie/atu/exam_summer/oop_exam_summer_50/BookingService/BookingService.java
@@ -1,4 +1,11 @@
 package ie.atu.exam_summer.oop_exam_summer_50.BookingService;
 
+import ie.atu.exam_summer.oop_exam_summer_50.BookingModel.BookingModel;
+import ie.atu.exam_summer.oop_exam_summer_50.BookingModel.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class BookingService {
 }
