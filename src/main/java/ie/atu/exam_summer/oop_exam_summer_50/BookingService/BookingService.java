14f04a1833530bde3ead41747ef3172572a14a65
CONORSHAUGHNESSY-STU <g00432599@atu.ie>
2026-03-02 12:21:49 +0000

    Booking Service - Start Made With Array For Booking Data

--- src/main/java/ie/atu/exam_summer/oop_exam_summer_50/BookingService/BookingService.java
+++ src/main/java/ie/atu/exam_summer/oop_exam_summer_50/BookingService/BookingService.java
@@ -5,7 +5,27 @@ import ie.atu.exam_summer.oop_exam_summer_50.BookingModel.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Service
 public class BookingService {
+    private List<BookingModel> bookings = new ArrayList<>();
+    private long bookingID = 1;
+
+    public BookingModel addBookingModel(BookingModel bookingModel) {
+        bookingModel.setBookingId(bookingID++);
+        bookings.add(bookingModel);
+        return bookingModel;
+    }
+
+    public List<BookingModel> getAllBookings() {
+        return bookings;
+    }
+
+    public List<BookingModel> getBookingByID(String ID) {
+        return bookings;
+    }
 }
