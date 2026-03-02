5f11c8b9e7654b43a0ae4653f1e2726d77f83ec3
CONORSHAUGHNESSY-STU <g00432599@atu.ie>
2026-03-02 11:24:14 +0000

    Initialised Model, Controller & Service Files

---
+++ src/main/java/ie/atu/exam_summer/oop_exam_summer_50/BookingModel/BookingModel.java
@@ -0,0 +1,7 @@
+package ie.atu.exam_summer.oop_exam_summer_50.BookingModel;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import io.swagger.v3.oas.annotations.media.Schema;
+public class BookingModel {
+}
---
+++ src/main/java/ie/atu/exam_summer/oop_exam_summer_50/BookingService/BookingService.java
@@ -0,0 +1,4 @@
+package ie.atu.exam_summer.oop_exam_summer_50.BookingService;
+
+public class BookingService {
+}
