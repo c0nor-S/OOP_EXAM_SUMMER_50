9852fff92452cfc80a8ffabc36e17a45cdfde77a
CONORSHAUGHNESSY-STU <g00432599@atu.ie>
2026-03-02 11:14:42 +0000

    First Commit

---
+++ src/main/java/ie/atu/exam_summer/oop_exam_summer_50/OopExamSummer50Application.java
@@ -0,0 +1,13 @@
+package ie.atu.exam_summer.oop_exam_summer_50;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopExamSummer50Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopExamSummer50Application.class, args);
+	}
+
+}
